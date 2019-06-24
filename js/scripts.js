eb21f731b3ba013b5d6a13da09c7aca8d8c7ccc5
Gavin Kimberlin <guviner@gmail.com>
2019-06-24 12:18:09 -0700

    add all files and add a basic template to the files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
-$(document).ready(function() {
-  $("#some-form").submit(function(event) {
+
+$(document).ready(function () {
+  $('#submit-button').submit(function (event) {
     event.preventDefault();
-
-    var someInputVar = $("#some-input").val(); // Don't forget it's a string without parseInt()
-    var someOutputVar = someInputVar;
-
-    $("#output").text(someOutputVar);
-    $("#output").show();
+    
   });
 });
